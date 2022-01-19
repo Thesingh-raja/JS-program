572c17fbb89cba3b3f55efb3c242441d7b24b135
Thesingh raja <thesingh.raja@codem.com>
2022-01-19 14:10:15 +0530

    Project-20-Number-Facts

---
+++ #20-number-facts-app/script.js
@@ -0,0 +1,95 @@
+//state to manage data
+
+class State {
+  state = {
+    loading: false,
+    number: "random",
+    type: "math",
+  };
+  updateState(props) {
+    this.state = {
+      ...this.state,
+      ...props,
+    };
+  }
+}
+
+const state = new State();
+
+const DOM = {
+  okBtn: document.querySelector(".okBtn"),
+  randomBtn: document.querySelector(".randomBtn"),
+  typeSelector: document.querySelector(".type"),
+  number: document.querySelector(".number"),
+  factbox: document.querySelector(".factbox"),
+};
+
+//event listeners 
+DOM.number.addEventListener("input", (e) => {
+  state.updateState({
+    number: e.target.value,
+  });
+  console.log(state.state);
+});
+
+DOM.typeSelector.addEventListener("change", (e) => {
+  state.updateState({
+    type: e.target.value,
+  });
+  console.log(state.state);
+});
+
+DOM.okBtn.addEventListener("click", () => {
+  state.updateState({ loading: true });
+  console.log("loading..");
+  showSpinner();
+  getFact(state.state.number, state.state.type)
+    .then(fact => {
+      console.log(fact);
+      state.updateState({ loading: true });
+    })
+    .catch(err => {
+      console.error(err);
+    })
+});
+
+DOM.randomBtn.addEventListener("click", () => {
+  state.updateState({ loading: true });
+  console.log("loading..")
+  showSpinner();
+  getFact("random", state.state.type)
+    .then(fact => {
+      console.log(fact);
+      state.updateState({ loading: true });
+    })
+    .catch(err => {
+      console.error(err);
+    })
+})
+
+//functions.
+const getFact = (number = "random", type = "math") => {
+  state.updateState({ loading: true });
+  return new Promise((resolve, reject) => {
+    fetch(`http://numbersapi.com/${number}/${type}`)
+      .then((res) => res.text())
+      .then((data) => {
+        showFact(data)
+        console.log(data);
+        state.updateState({ loading: false });
+        resolve(data);
+      })
+      .catch((err) => {
+        state.updateState({ loading: false });
+        reject(err);
+      });
+  });
+};
+
+const showFact = (fact) => {
+  DOM.factbox.innerHTML = fact;
+};
+
+const showSpinner = () => {
+  DOM.factbox.innerHTML = `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`;
+}
