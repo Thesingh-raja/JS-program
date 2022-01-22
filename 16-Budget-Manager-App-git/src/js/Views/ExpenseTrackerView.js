798bb7f9f49e720d2098356894196052602fd7f0
Thesingh raja <thesingh.raja@codem.com>
2022-01-22 12:45:58 +0530

    Project-16-Budget-Manager-App

---
+++ 16-Budget-Manager-App-git/src/js/Views/ExpenseTrackerView.js
@@ -0,0 +1,8 @@
+import { ListView } from "./ListView";
+
+class ExpenseTrackerView extends ListView {
+    _container = document.querySelector(".expenses_container")
+    _filterSelect = document.querySelector(".expense_filter");  
+};
+
+export default new ExpenseTrackerView();
